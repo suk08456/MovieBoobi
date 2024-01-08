343c5499138ad90d26e7bf46d0ed866d1621e21a
thisisnine99 <dkdlel2099@gmail.com>
2024-01-08 10:15:53 +0900

    book추가완료

--- src/main/java/com/korea/MOVIEBOOK/book/BookController.java
+++ src/main/java/com/korea/MOVIEBOOK/book/BookController.java
@@ -3,9 +3,7 @@ package com.korea.MOVIEBOOK.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -38,43 +36,17 @@ public class BookController {
             endIndex+=5;
         }
         System.out.println("=================================== 새로고침 ===================================");
-        model.addAttribute("bestSellerListList", bestSellerListList);
-        model.addAttribute("newSpecialBookListList", newSpecialBookListList);
+        List<List<List<Book>>> allList = new ArrayList<>();
+        allList.add(bestSellerListList);
+        allList.add(newSpecialBookListList);
+        model.addAttribute("allList", allList);
         return "book/bookMainPage";
     }
 
-    @GetMapping("/test")
-    public String test(Model model) {
-        List<Book> bestSellerList = bookService.getBestSellerList();
-        bestSellerList.sort(Comparator.comparing(Book::getBestRank));
-        List<List<Book>> bestSellerListList = new ArrayList<>();
-        int startIndex = 0;
-        int endIndex = 5;
-        for (int i = 1; i <= bestSellerList.size()/5; i++) {
-            bestSellerListList.add(bestSellerList.subList(startIndex, Math.min(endIndex, bestSellerList.size())));
-            startIndex+=5;
-            endIndex+=5;
-        }
-        startIndex = 0;
-        endIndex = 5;
-        List<Book> newSpecialBookList = bookService.getNewSpecialBookList();
-        List<List<Book>> newSpecialBookListList = new ArrayList<>();
-        for (int i = 1; i <= newSpecialBookList.size()/5; i++) {
-            newSpecialBookListList.add(newSpecialBookList.subList(startIndex, Math.min(endIndex, newSpecialBookList.size())));
-            startIndex+=5;
-            endIndex+=5;
-        }
-        System.out.println("=================================== 새로고침 ===================================");
-        model.addAttribute("bestSellerListList", bestSellerListList);
-        model.addAttribute("newSpecialBookListList", newSpecialBookListList);
-        return "book/bookTest";
+    @PostMapping("/detail")
+    public String detailPage(String title, Model model) {
+        model.addAttribute("title", title);
+        bookService.findByTitle(title);
+        return "book/testtest";
     }
-
-    @GetMapping("/detail")
-    @ResponseBody
-    public String detailPage() {
-
-        return "안녕하세요";
-    }
-
 }
