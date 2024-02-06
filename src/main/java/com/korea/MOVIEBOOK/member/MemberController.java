b503782067a3c6509c1748610ad2c1d90850cc69
[suk08456] <[jangchanik0823@gmail.com]>
2024-02-06 11:21:30 +0900

    project240206

--- src/main/java/com/korea/MOVIEBOOK/member/MemberController.java
+++ src/main/java/com/korea/MOVIEBOOK/member/MemberController.java
@@ -212,7 +212,6 @@ public class MemberController {
         Long reviewCount = reviewService.getReivewCount(member);
 
         model.addAttribute("sum", sum);
-
         model.addAttribute("reviewCount", reviewCount);
         model.addAttribute("parameter", 0);
 
